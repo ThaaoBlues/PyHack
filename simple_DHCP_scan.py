87f39f321adc23c6101ad2c9edcc3d77a4ef8155
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2020-09-22 20:52:27 +0200

    don't need scapy anymore, visual improved

--- simple_DHCP_scan.py
+++ simple_DHCP_scan.py
@@ -1,44 +1,40 @@
-from scapy.all import * 
+
 import socket
 from io import StringIO
-
+from multiprocessing import process, freeze_support
+from subprocess import check_output, STDOUT
+import res
+import sys
 class scanner():
-    def __init__(self,ip_range):
-        if ip_range == "all":
-            hostname = socket.gethostname()
-            ip = socket.gethostbyname_ex(hostname)
-            ip = str(ip[-1][-1])
-            print("YOU :: {} :: {}".format(hostname,ip))
-            base_ip = ip.split(sep=".")
-            base = "{}.{}.{}.1/24".format(base_ip[0],base_ip[1],base_ip[2])
-            self.scan(base,base_ip[0])
+    def __init__(self):
 
-        else:
-            self.scan(ip_range)
+        hostname = socket.gethostname()
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        print("YOU :: {} :: {}".format(hostname,ip))
+        base_ip = ip.split(sep=".")
+        base = "{}.{}.{}.".format(base_ip[0],base_ip[1],base_ip[2])
+        self.scan(base)
 
 
-    def scan(self,ip,first_3):
-        output = []
-        i = 0
-        with Capturing(output) as output:
-            arping(ip)
-        for line in output:
-            lines = line.split()
-            for addr in lines:
-                if addr[:3] in first_3:
-                    i += 1
-                    print("[{}] {} :: {} ".format(i,line,str(socket.gethostbyaddr(addr)[0])))
+    def scan(self,ip):
+    
+        for i in range(255):
+            if i == 0:
+                pass    
+            else:
+                cmd = f"ping -c 1 {ip}{i}".split()
+                try:
+                    r = check_output(cmd, shell=False, stderr = STDOUT)
+                    if "1 received" in str(r):
+                        print("[{}] {} :: {} ".format(str(i),ip+str(i),str(socket.gethostbyaddr(ip+str(i)))))
+                except:
+                    sys.stdout.write(f"\r{i}/255")
+                
 
 
-class Capturing(list):
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
-        return self
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
-        sys.stdout = self._stdout
 
 if __name__ == "__main__":
-    scan = scanner("all")
+    freeze_support()
+    scan = scanner()
