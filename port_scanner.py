431ded2449d4d3ca61dd38dabe9e6e78493bb10a
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2020-09-23 19:00:34 +0200

    FUCKING FAST PORT SCANNER YAY

---
+++ port_scanner.py
@@ -0,0 +1,27 @@
+import socket
+from multiprocessing import Process, freeze_support
+
+class port_scanner():
+    
+    def __init__(self):
+        pass
+
+    def scan(self,ip,max=8888):
+        print(f"[+] beginning scan... ({max} ports) ")
+        for i in range(max):
+            proc = Process(target=self.connect, args =(ip,i,))
+            proc.start()
+    
+    def connect(self,ip,port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            print(f"\r[+]Trying port number {port}", end="")
+            sock.connect((ip,port))
+            print(f"\r=====[+] OPEN PORT : {port}=====\n",end="")
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    ps = port_scanner()
+    ps.scan("192.168.8.1",max=2000)
